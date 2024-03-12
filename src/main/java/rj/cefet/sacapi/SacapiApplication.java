ce435be05754bae7e057105bbf86f8132b8bb452
Igor Rodrigues <77209950+Igor-da-SilvaRodrigues@users.noreply.github.com>
2024-03-11 22:36:02 -0300

    Commit inicial

---
+++ src/main/java/rj/cefet/sacapi/SacapiApplication.java
@@ -0,0 +1,13 @@
+package rj.cefet.sacapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SacapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SacapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/rj/cefet/sacapi/SacapiApplicationTests.java
@@ -0,0 +1,13 @@
+package rj.cefet.sacapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SacapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
