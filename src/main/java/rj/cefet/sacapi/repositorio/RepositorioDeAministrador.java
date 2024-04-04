1d9848dbe0308aaeb4f947bdae8f7c1ba6e9adb3
Igor Rodrigues <77209950+Igor-da-SilvaRodrigues@users.noreply.github.com>
2024-04-04 20:12:58 -0300

    Ajustar repositorios
    Ajustar repositorios para permitir resgatar UserDetails a partir da chave primaria.

--- src/main/java/rj/cefet/sacapi/repositorio/RepositorioDeAministrador.java
+++ src/main/java/rj/cefet/sacapi/repositorio/RepositorioDeAministrador.java
@@ -1,9 +1,11 @@
 package rj.cefet.sacapi.repositorio;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.UserDetails;
 import rj.cefet.sacapi.modelo.Administrador;
 
 import java.util.UUID;
 
-public interface RepositorioDeAministrador extends JpaRepository<Administrador, UUID> {
+public interface RepositorioDeAministrador extends JpaRepository<Administrador, String> {
+    UserDetails findByMatricula(String matricula);
 }
