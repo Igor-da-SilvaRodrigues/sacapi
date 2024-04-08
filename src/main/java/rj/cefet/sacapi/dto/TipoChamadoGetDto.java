93523c0dc6ac672af49d390cd08ee2bbe1577f8b
Igor Rodrigues <77209950+Igor-da-SilvaRodrigues@users.noreply.github.com>
2024-04-08 19:09:17 -0300

    Ajustando dtos
    Ajustando os DTOs de TipoChamado para refletir as mudanças nos modelos

--- src/main/java/rj/cefet/sacapi/dto/TipoChamadoGetDto.java
+++ src/main/java/rj/cefet/sacapi/dto/TipoChamadoGetDto.java
@@ -4,13 +4,18 @@ import rj.cefet.sacapi.modelo.Motivo;
 import rj.cefet.sacapi.modelo.TipoChamado;
 
 import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
 
-public record TipoChamadoGetDto(String tipo, Integer prioridade, List<String> motivos, Boolean arquivado) {
+public record TipoChamadoGetDto(String tipo, Integer prioridade, Set<String> motivos, Boolean arquivado) {
+    /**
+     * Converte um TipoChamado em um dto
+     */
     public static TipoChamadoGetDto fromTipoChamado(TipoChamado tipoChamado){
         return new TipoChamadoGetDto(
                 tipoChamado.getTipo(),
                 tipoChamado.getPrioridade(),
-                tipoChamado.getMotivoList().stream().map(Motivo::asString).toList(),
+                tipoChamado.getMotivoSet().stream().map(Motivo::asString).collect(Collectors.toSet()),
                 tipoChamado.isArquivado()
         );
     }
